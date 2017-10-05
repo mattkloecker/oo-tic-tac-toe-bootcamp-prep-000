f45a9fbdded0696680b82fd5e15f74e0ef5d6cf4
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-10-05 15:24:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,21 @@
 class TicTacToe
 
+ #instance of the game sets a starting board aka empty array
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
 
+  #combinations of wins
+  WIN_COMBINATIONS = [
+    [0,1,2], #top row win
+    [3,4,5], #middle row win
+    [6,7,8], #bottom row win
+    [0,3,6], #left column win
+    [1,4,7], #middle column win
+    [2,5,8], #right column win
+    [0,4,8], #left diagnonal win
+    [2,4,6], #right diagonal win
+  ]
 
 
 
