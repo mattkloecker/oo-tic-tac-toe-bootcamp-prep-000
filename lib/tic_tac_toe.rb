82f74261ca9987229d350c4b30055491a962f50d
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-10-05 15:28:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,9 +19,9 @@ class TicTacToe
 
 #displays the board
   def display_board
-    rows = [" #{board[0]} | #{board[1]} | #{board[2]} ",\
-    " #{board[3]} | #{board[4]} | #{board[5]} ",\
-    " #{board[6]} | #{board[7]} | #{board[8]} "]
+    rows = [" #{@board[0]} | #{@board[1]} | #{@board[2]} ",\
+    " #{@board[3]} | #{@board[4]} | #{@board[5]} ",\
+    " #{@board[6]} | #{@board[7]} | #{@board[8]} "]
     puts "#{rows[0]}\n-----------\n#{rows[1]}\n-----------\n#{rows[2]}"
 
   end
